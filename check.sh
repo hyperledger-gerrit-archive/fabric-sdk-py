3df40573a165fd9bcc3c7888926e6b7b3318fd04
grapebaba <281165273@qq.com>
2016-12-28 17:33:10 +0800

    Enable docker-compose for test environment
    Fix FAB-1483.
    
    Change-Id: Ib74d5fded9ea98f88f21f6c4a791b93cda3f9088
    Signed-off-by: grapebaba <281165273@qq.com>

---
+++ check.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# clean env
+echo "Clean env..."
+docker-compose -f test/docker-compose-test.yml stop
+docker-compose -f test/docker-compose-test.yml kill
+docker-compose -f test/docker-compose-test.yml rm -f
+
+# start env and wait until services started
+echo "Setup env..."
+docker pull tcz001/hyperledger-fabric-cop
+docker pull tcz001/hyperledger-fabric-peer
+docker pull tcz001/hyperledger-fabric-orderer
+docker tag tcz001/hyperledger-fabric-cop:latest hyperledger/fabric-cop:latest
+docker tag tcz001/hyperledger-fabric-peer:latest hyperledger/fabric-peer:latest
+docker tag tcz001/hyperledger-fabric-orderer:latest hyperledger/fabric-order:latest
+
+docker-compose -f test/docker-compose-test.yml up -d
+
+sleep 10
+
+# run tests
+echo "Starting test..."
+docker-compose exec sdk-py make test
+
+# clean env
+docker-compose -f test/docker-compose-test.yml stop
+docker-compose -f test/docker-compose-test.yml kill
+docker-compose -f test/docker-compose-test.yml rm -f
