c5a9f375aed28baea5fa99f06f10f9e0e19139ba
grapebaba <281165273@qq.com>
2017-03-03 17:58:28 +0800

    Pass install chaincode test
    Good news right now we pass lastet install chaincode
    test.
    
    Change-Id: I0c3789d34a66dd33f3ea1e9caf69c5b8f2fd9db8
    Signed-off-by: grapebaba <281165273@qq.com>

--- check.sh
+++ check.sh
@@ -8,15 +8,15 @@ docker-compose -v
 # pull fabric images
 IMG_TAG=0.8.4
 echo "===Pulling fabric images... with tag = ${IMG_TAG}"
-docker pull yeasy/hyperledger-fabric-base:$IMG_TAG
-docker pull yeasy/hyperledger-fabric-peer:$IMG_TAG
-docker pull yeasy/hyperledger-fabric-orderer:$IMG_TAG
+docker pull yeasy/hyperledger-fabric-base:latest
+docker pull yeasy/hyperledger-fabric-peer:latest
+docker pull yeasy/hyperledger-fabric-orderer:latest
 docker pull yeasy/hyperledger-fabric-ca:$IMG_TAG
 
-docker tag yeasy/hyperledger-fabric-base:$IMG_TAG hyperledger/fabric-baseimage
-docker tag yeasy/hyperledger-fabric-base:$IMG_TAG hyperledger/fabric-ccenv:x86_64-1.0.0-snapshot-preview
-docker tag yeasy/hyperledger-fabric-peer:$IMG_TAG hyperledger/fabric-peer
-docker tag yeasy/hyperledger-fabric-orderer:$IMG_TAG hyperledger/fabric-orderer
+docker tag yeasy/hyperledger-fabric-base:latest hyperledger/fabric-baseimage
+docker tag yeasy/hyperledger-fabric-base:latest hyperledger/fabric-ccenv:x86_64-1.0.0-snapshot-preview
+docker tag yeasy/hyperledger-fabric-peer:latest hyperledger/fabric-peer
+docker tag yeasy/hyperledger-fabric-orderer:latest hyperledger/fabric-orderer
 docker tag yeasy/hyperledger-fabric-ca:$IMG_TAG hyperledger/fabric-ca
 
 # run tests
