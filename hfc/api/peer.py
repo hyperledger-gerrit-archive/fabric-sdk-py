051e922455bdbca91675826bf3ff9c0e14625cdf
grapebaba <281165273@qq.com>
2017-03-14 17:49:17 +0800

    [FAB-1044]Impl orderer broadcast and delivery
    Change-Id: I3bef2e5afb2cb405a69321446f89f60ced368857
    Signed-off-by: grapebaba <281165273@qq.com>

--- hfc/api/peer.py
+++ hfc/api/peer.py
@@ -1,3 +1,17 @@
+# Copyright IBM Corp. 2017 All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 import logging
 
 from hfc.protos.peer import peer_pb2_grpc
@@ -19,14 +33,13 @@ class Peer(object):
         self._endorser_client = peer_pb2_grpc.EndorserStub(
             channel(self._endpoint, pem, opts))
 
-    def send_proposal(self, proposal, scheduler=None):
+    def send_proposal(self, proposal):
         """ Send an endorsement proposal to endorser
 
         Args:
             proposal: The endorsement proposal
-            scheduler: Scheduler, see rx.concurrency
 
-        Returns: An rx.Observable of proposal_response or exception
+        Returns: proposal_response or exception
 
         """
         _logger.debug("Send proposal={}".format(proposal))
