ee6ca0469baa89fbe9c13c135dcc305a66e97fc7
grapebaba <281165273@qq.com>
2017-07-11 17:41:55 +0800

    [FAB-5151]Install chaincode
    This patchset refactor install chaincode interface.
    Introduce one design that is make TX_CONEXT object
    as a common argument for all methods related
    transaction operation. Nowthat only support golang
    chaincode.
    
    Change-Id: I0a38cd93ae976fab8096909827f4f93a3b721670
    Signed-off-by: grapebaba <281165273@qq.com>

--- hfc/fabric/peer.py
+++ hfc/fabric/peer.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 #
 import logging
+import threading
 
 import rx
 
@@ -27,10 +28,12 @@ _logger = logging.getLogger(__name__ + ".peer")
 class Peer(object):
     """ A peer node in the network.
 
-    It has a specific Grpc channel address.
+    It has a specific grpc channel address.
     """
 
     def __init__(self, endpoint=DEFAULT_PEER_ENDPOINT, pem=None, opts=None):
+        self._lock = threading.RLock()
+        self._channels = []
         self._endpoint = endpoint
         self._endorser_client = peer_pb2_grpc.EndorserStub(
             channel(self._endpoint, pem, opts))
@@ -39,6 +42,7 @@ class Peer(object):
         """ Send an endorsement proposal to endorser
 
         Args:
+            scheduler: rx scheduler
             proposal: The endorsement proposal
 
         Returns: proposal_response or exception
@@ -57,3 +61,32 @@ class Peer(object):
 
         """
         return self._endpoint
+
+    def join(self, chan):
+        """ Join a channel
+
+        Args:
+            chan: a channel instance
+
+        """
+        with self._lock:
+            self._channels.append(chan)
+
+    @property
+    def channels(self):
+        with self._lock:
+            return self._channels
+
+
+def create_peer(endpoint=DEFAULT_PEER_ENDPOINT, pem=None, opts=None):
+    """ Factory method to construct a peer instance
+
+    Args:
+        endpoint: endpoint
+        pem: pem
+        opts: opts
+
+    Returns: a peer instance
+
+    """
+    return Peer(endpoint, pem, opts)
