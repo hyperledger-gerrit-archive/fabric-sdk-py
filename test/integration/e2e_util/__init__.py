7c6efa2a92c4a76d6d07c3c8c91ba1c8ae5d532e
Wang Dong <xdragon007@gmail.com>
2017-10-06 16:03:32 +0800

    Refactor the e2e test framework
    Change-Id: I6a0144aba88dcc85ded26e9b3d24acaaede9f297
    Signed-off-by: Wang Dong <xdragon007@gmail.com>

---
+++ test/integration/e2e_util/__init__.py
@@ -0,0 +1,11 @@
+"""
+# Copyright IBM Corp. 2017 All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+"""
+
+# flake8: noqa
+
+__all__ = [ 'create_channel']
+
+from test.integration.e2e_util.create_channel import create_channel_request
