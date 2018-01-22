hyperledger-gerrit-archive/fabric-sdk-py

commit 9a7d9e9f0f4e3a506beafef7602afc2bd46883a5

FAB-5488: e2e join channel test
1. implement the channel joining feature
2. implement getting genesis block feature
3. test the e2e join channel feature
4. clean the unused code in channel.

Change-Id: I7cf1062a4b30b016d7c7aeed0dc39df24af2c688
Signed-off-by: Wang Dong <xdragon007@gmail.com>

hfc/fabric/channel/channel.py
@@ -16,12 +16,23 @@ from hfc.fabric.transaction.tx_proposal_request import \
 from hfc.protos.common import common_pb2
 from hfc.protos.orderer import ab_pb2
 from hfc.protos.peer import chaincode_pb2, proposal_pb2
+from hfc.protos.utils import create_seek_info, create_chaincode_spec, \
+    create_seek_payload, create_envelope
 from hfc.util import utils
-from hfc.util.utils import proto_str, current_timestamp, proto_b
+from hfc.util.utils import proto_str, current_timestamp, proto_b, \
+    build_header, build_channel_header, build_cc_proposal, \
+    create_serialized_identity, send_transaction_proposal
+
+if sys.version_info < (3, 0):
+    from Queue import Queue
+else:
+    from queue import Queue
+
 
 SYSTEM_CHANNEL_NAME = ""
 
 _logger = logging.getLogger(__name__ + ".channel")
+_logger.setLevel(logging.DEBUG)
 
 
 class Channel(object):
@@ -355,23 +366,6 @@ class Channel(object):
         """
         return True
 
-    def update(self, config, orderer, signers):
-        """Update a channel configuration
-
-        Calls the orderer(s) to update an existing channel. This allows the
-        addition and deletion of Peer nodes to an existing channel, as well as
-        the update of Peer certificate information upon certificate renewals.
-
-        Args:
-            config: config to be updated
-            orderer: specific orderer to use
-            signers: the Ecert of users
-        Returns: True if the channel update process was successful,
-            False otherwise.
-
-        """
-        return True
-
     def is_readonly(self):
         """Check the channel if read-only
 
@@ -416,62 +410,6 @@ class Channel(object):
         """
         pass
 
-    @staticmethod
-    def instantiate(name, config, signers, orderer):
-        """
-        This function will be the factory function to create
-        a really new channel.
-
-        Args:
-            name: channel name
-            config: configuration class instance
-            config_signed(string): signed config file
-            orderer: orderer instance
-        Return: True successfully or False in failure
-        """
-        pass
-
-    def _update_or_create(self, config, signers, orderer):
-        """
-        Send configuration to orderer for updating.
-        This function really does all the dirty work with remote orderer.
-        This is the low level function to create or update function.
-
-        Args:
-            conifg(string):  channel config
-            signers(list):   list of user Ecert
-            orderer:         Orderer instance
-        Return: True if updated sucessfully, False otherwise
-
-        """
-        pass
-
-    def _get_config_payload(self, config, signers, is_update):
-        """
-        This function will build the config payload sent to
-        orderer.
-        Args:
-            config: channel config
-            user:   user client
-        Return:
-             the config payload for the orderer, None if failure.
-
-        """
-        pass
-
-    def _get_config_sigs(self, config, signers):
-        """
-        This function is used to sign the config with user or
-        to update the user signature.
-
-        Args:
-            config: channel configuration instance to be signed
-            user:   user to signed the config
-            is_update: wether to update the config
-        Return: signed config in string
-        """
-        pass
-
     def _package_chaincode(self, cc_path, cc_type):
         """ Package all chaincode env into a tar.gz file
 
@@ -511,6 +449,119 @@ class Channel(object):
 
         else:
             raise ValueError('Currently only support install GOLANG chaincode')
+
+    def get_genesis_block(self):
+        """ get the genesis block of the channel.
+        Return: the genesis block in success or None in fail.
+        """
+        _logger.info("get genesis block - start")
+
+        orderer = self._get_random_orderer()
+
+        tx_context = self._client.tx_context
+
+        seek_info = create_seek_info(0, 0)
+        seek_info_header = build_channel_header(
+            common_pb2.HeaderType.Value('DELIVER_SEEK_INFO'),
+            tx_context.tx_id,
+            self.name,
+            current_timestamp(),
+            tx_context.epoch)
+
+        seek_header = build_header(
+            tx_context.identity,
+            seek_info_header,
+            tx_context.nonce)
+
+        seek_payload_bytes = create_seek_payload(seek_header, seek_info)
+        sig = tx_context.sign(seek_payload_bytes)
+
+        envelope = create_envelope(sig, seek_payload_bytes)
+        q = Queue(1)
+        response = orderer.delivery(envelope)
+        response.subscribe(on_next=lambda x: q.put(x),
+                           on_error=lambda x: q.put(x))
+
+        res, _ = q.get(timeout=5)
+
+        if res.block is None or res.block == '':
+            _logger.error("fail to get genesis block")
+            return None
+
+        _logger.info("get genesis block successfully, block=%s",
+                     res.block.header)
+        return res.block
+
+    def join_channel(self, request):
+        """
+        To join the peer to a channel.
+
+        Args:
+            request: the request to join a channel
+        Return:
+            True in sucess or False in failure
+        """
+        _logger.debug('join_channel - start')
+
+        err_msg = None
+        if(not request):
+            err_msg = "Missing all required input request parameters"
+
+        if 'targets'not in request:
+            err_msg = "Missing peers parameter"
+
+        if 'block' not in request:
+            err_msg = "Missing genesis block parameter"
+
+        if 'tx_id' not in request:
+            err_msg = "Missing transaction id parameter"
+
+        if err_msg:
+            _logger.error('join_channel error: {}'.format(err_msg))
+            raise ValueError(err_msg)
+
+        tx_context = self._client.tx_context
+        chaincode_spec = create_chaincode_spec(
+            request['block'],
+            'cscc',
+            "GOLANG")
+
+        extension = chaincode_spec.SerializeToString()
+        channel_header = build_channel_header(
+            common_pb2.HeaderType.Value('ENDORSER_TRANSACTION'),
+            tx_context.tx_id,
+            self.name,
+            current_timestamp(),
+            tx_context.epoch,
+            extension=extension
+        )
+
+        creator = create_serialized_identity(tx_context.user)
+        header = build_header(creator, channel_header, tx_context.nonce)
+        proposal = build_cc_proposal(
+            chaincode_spec,
+            header,
+            {})
+
+        try:
+            responses = send_transaction_proposal(
+                proposal,
+                header,
+                tx_context,
+                request['targets'])
+        except Exception as e:
+            raise IOError("fail to send transanction proposal", e)
+
+        q = Queue(1)
+        for r in responses:
+            r.subscribe(on_next=lambda x: q.put(x),
+                        on_error=lambda x: q.put(x))
+            res = q.get(timeout=5)
+
+            result = True and (res.response.status == 200)
+
+        _logger.info("The return status is:", result)
+        return result
 
 
 def create_system_channel(client):

hfc/protos/utils.py
@@ -1,0 +1,82 @@
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from hfc.protos.orderer import ab_pb2
+from hfc.protos.common import common_pb2
+from hfc.protos.peer import chaincode_pb2, transaction_pb2
+
+def create_seek_info(start, stop, behavior="BLOCK_UNTIL_READY"):
+
+     # build start
+    seek_specified_start = ab_pb2.SeekSpecified()
+    seek_specified_start.number = start
+    seek_start = ab_pb2.SeekPosition()
+    seek_start.specified.CopyFrom(seek_specified_start)
+
+    # build stop
+    seek_specified_stop = ab_pb2.SeekSpecified()
+    seek_specified_stop.number = stop
+    seek_stop = ab_pb2.SeekPosition()
+    seek_stop.specified.CopyFrom(seek_specified_stop)
+
+    # seek info with all parts
+    seek_info = ab_pb2.SeekInfo()
+    seek_info.start.CopyFrom(seek_start)
+    seek_info.stop.CopyFrom(seek_stop)
+    seek_info.behavior = \
+            ab_pb2.SeekInfo.SeekBehavior.Value(behavior)
+
+    return seek_info
+
+def create_seek_payload(seek_header, seek_info):
+
+    seek_payload = common_pb2.Payload()
+    seek_payload.header.CopyFrom(seek_header)
+    seek_payload.data = seek_info.SerializeToString()
+    seek_payload_bytes = seek_payload.SerializeToString()
+
+    return seek_payload_bytes
+
+def create_chaincode_spec(block_bytes, chaincode_id_name, type):
+
+    chaincode_input = chaincode_pb2.ChaincodeInput()
+    chaincode_input.args.append(block_bytes)
+
+    chaincode_id = chaincode_pb2.ChaincodeID()
+    chaincode_id.name = chaincode_id_name
+
+    chaincode_spec = chaincode_pb2.ChaincodeSpec()
+    chaincode_spec.type = chaincode_pb2.ChaincodeSpec.Type.Value(type)
+    chaincode_spec.chaincode_id.CopyFrom(chaincode_id)
+    chaincode_spec.input.CopyFrom(chaincode_input)
+
+    return chaincode_spec.SerializeToString()
+
+def create_tx_payload(endorsements, tran_req):
+
+    cc_action_payload = transaction_pb2.ChaincodeActionPayload()
+    response, _ = tran_req.responses[0]
+    cc_action_payload.action.proposal_response_payload = \
+        response.payload
+    cc_action_payload.action.endorsements.extend(endorsements)
+    cc_action_payload.chaincode_proposal_payload = tran_req.proposal.payload
+
+    tran = transaction_pb2.Transaction()
+    cc_tran_action = tran.actions.add()
+    cc_tran_action.header = tran_req.header.signature_header
+    cc_tran_action.payload = cc_action_payload.SerializeToString()
+    tran_payload = common_pb2.Payload()
+    tran_payload.header.channel_header = tran_req.header.channel_header
+    tran_payload.header.signature_header = tran_req.header.signature_header
+    tran_payload.data = tran.SerializeToString()
+
+    tran_payload_bytes = tran_payload.SerializeToString()
+    return tran_payload_bytes
+
+def create_envelope(seek_payload_sig, seek_payload_bytes):
+
+    envelope = common_pb2.Envelope()
+    envelope.signature = seek_payload_sig
+    envelope.payload = seek_payload_bytes
+
+    return envelope

test/integration/e2e_test.py
@@ -9,15 +9,19 @@ import unittest
 import logging
 import os
 import sys
+import time
 
 from hfc.fabric.client import Client
 from hfc.util.keyvaluestore import FileKeyValueStore
 
 from test.unit.util import cli_call
 from test.integration.config import E2E_CONFIG
-from test.integration.e2e_utils import build_channel_request
+from test.integration.e2e_utils import build_channel_request, \
+    build_join_channel_req
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+test_network = E2E_CONFIG['test-network']
 
 if sys.version_info < (3, 0):
     from Queue import Queue
@@ -31,14 +35,12 @@ class E2eTest(unittest.TestCase):
 
         self.base_path = "/tmp/fabric-sdk-py"
         self.kv_store_path = os.path.join(self.base_path, "key-value-store")
-        self.client = Client()
-        self.client.state_store = FileKeyValueStore(self.kv_store_path)
         self.channel_tx = \
-            E2E_CONFIG['test-network']['channel-artifacts']['channel.tx']
+            test_network['channel-artifacts']['channel.tx']
         self.channel_name = \
-            E2E_CONFIG['test-network']['channel-artifacts']['channel_id']
+            test_network['channel-artifacts']['channel_id']
         self.compose_file_path = \
-            E2E_CONFIG['test-network']['docker']['compose_file_tls']
+            test_network['docker']['compose_file_tls']
 
         self.start_test_env()
 
@@ -57,31 +59,49 @@ class E2eTest(unittest.TestCase):
 
     def create_channel(self):
 
+        client = Client()
+        client.state_store = FileKeyValueStore(
+            self.kv_store_path + 'build-channel')
+
         logger.info("start to create channel")
         request = build_channel_request(
-            self.client,
+            client,
             self.channel_tx,
             self.channel_name)
 
         q = Queue(1)
-        response = self.client.create_channel(request)
+        response = client.create_channel(request)
         response.subscribe(on_next=lambda x: q.put(x),
                            on_error=lambda x: q.put(x))
 
         status, _ = q.get(timeout=5)
 
-        try:
-            self.assertEqual(status.status, 200)
-        except AssertionError:
-            logger.error("fail to create the channel with status code",
-                         status.status)
-            raise Exception("fail to create channel.")
+        self.assertEqual(status.status, 200)
 
-        logger.info("successfully create the channel")
+        logger.info("successfully create the channel: %s", self.channel_name)
+        client.state_store = None
 
     def join_channel(self):
 
-        pass
+        # sleep 5 seconds for channel created
+        time.sleep(5)
+        client = Client()
+        client.state_store = FileKeyValueStore(
+            self.kv_store_path + 'join-channel')
+
+        channel = client.new_channel(self.channel_name)
+
+        logger.info("start to join channel")
+        orgs = ["org1.example.com", "org2.example.com"]
+        for org in orgs:
+            request = build_join_channel_req(org, channel, client)
+            assert(request)
+            # result = True and channel.join_channel(request)
+            logger.info("peers in org: %s join channel: %",
+                        org, self.channel_name)
+
+        logger.info("joining channel tested succefully")
+        client.state_store = None
 
     def install_chaincode(self):
 

test/integration/e2e_utils.py
@@ -4,16 +4,18 @@
 #
 """
 
-
 from hfc.fabric.orderer import Orderer
+from hfc.fabric.peer import Peer
+from hfc.fabric.eventhub import EventHub
 from hfc.fabric.transaction.tx_context import TXContext
 from hfc.fabric.transaction.tx_proposal_request import TXProposalRequest
-from hfc.util.crypto.crypto import Ecies
+from hfc.util.crypto.crypto import ecies
 from hfc.util import utils
 
 from test.unit.util import get_orderer_org_admin, get_peer_org_admin
 
 from test.integration.config import E2E_CONFIG
+test_network = E2E_CONFIG['test-network']
 
 
 def build_channel_request(client, channel_tx, channel_name):
@@ -42,7 +44,7 @@ def build_channel_request(client, channel_tx, channel_name):
         opts=opts
     )
     orderer_admin = get_orderer_org_admin(client)
-    orderer_tx_context = TXContext(orderer_admin, Ecies(), prop_req, {})
+    orderer_tx_context = TXContext(orderer_admin, ecies(), prop_req, {})
     client.tx_context = orderer_tx_context
     orderer_admin_signature = client.sign_channel_config(config)
     orderer_admin_signature_bytes = orderer_admin_signature.SerializeToString()
@@ -51,7 +53,7 @@ def build_channel_request(client, channel_tx, channel_name):
     nonce = orderer_tx_context.nonce
 
     org1_admin = get_peer_org_admin(client, 'org1.example.com')
-    org1_tx_context = TXContext(org1_admin, Ecies(), prop_req, {})
+    org1_tx_context = TXContext(org1_admin, ecies(), prop_req, {})
     client.tx_context = org1_tx_context
     org1_admin_signature = client.sign_channel_config(config)
     org1_admin_signature_bytes = org1_admin_signature.SerializeToString()
@@ -59,7 +61,7 @@ def build_channel_request(client, channel_tx, channel_name):
     signatures.append(org1_admin_signature_bytes)
 
     org2_admin = get_peer_org_admin(client, 'org2.example.com')
-    org2_tx_context = TXContext(org2_admin, Ecies(), prop_req, {})
+    org2_tx_context = TXContext(org2_admin, ecies(), prop_req, {})
     client.tx_context = org2_tx_context
     org2_admin_signature = client.sign_channel_config(config)
     org2_admin_signature_bytes = org2_admin_signature.SerializeToString()
@@ -71,5 +73,86 @@ def build_channel_request(client, channel_tx, channel_name):
                'orderer': orderer,
                'tx_id': tx_id,
                'nonce': nonce}
+
+    return request
+
+
+def disconnect(all_ehs):
+    """
+    disconnect the eventhubs if connected
+    Args:
+        all_ehs: all the event hubs
+    Return: no return value
+    """
+    for eh in all_ehs:
+        if eh.is_connected:
+            eh.disconnect()
+
+
+def build_join_channel_req(org, channel, client):
+    """
+    For test, there is only one peer.
+
+    Args:
+        org: org
+        channel: the channel to join
+        client: client instance
+    Return:
+        return request for joining channel
+        """
+
+    def block_event_callback(block):
+
+        pass
+
+    client._crypto_suite = ecies()
+    all_ehs = []
+    request = {}
+    tx_prop_req = TXProposalRequest()
+
+    # add the orderer
+    orderer_config = test_network['orderer']
+    endpoint = orderer_config['grpc_endpoint']
+    opts = (('grpc.ssl_target_name_override',
+             orderer_config['server_hostname']),)
+
+    ca_root_path = orderer_config['tls_cacerts']
+    with open(ca_root_path, 'rb') as f:
+        pem = f.read()
+    orderer = Orderer(endpoint=endpoint, pem=pem, opts=opts)
+    channel.add_orderer(orderer)
+
+    # get the genesis block
+    orderer_admin = get_orderer_org_admin(client)
+    tx_context = TXContext(orderer_admin, ecies(), tx_prop_req)
+    client.tx_context = tx_context
+    genesis_block = channel.get_genesis_block().SerializeToString()
+    if not genesis_block:
+        return None
+
+    # create the peer
+    org_admin = get_peer_org_admin(client, org)
+    client.tx_context = TXContext(org_admin, ecies(), tx_prop_req)
+
+    peer_config = test_network[org]["peers"]['peer0']
+    ca_root = peer_config["tls_cacerts"]
+    with open(ca_root, 'rb') as f:
+        pem = f.read()
+    peer = Peer(pem=pem, opts=None)
+
+    # connect the peer
+    eh = EventHub()
+    event = peer_config['grpc_event_endpoint']
+    opts = {'pem': pem, 'hostname': peer_config['server_hostname']}
+
+    tx_id = client.tx_context.tx_id
+    eh.set_peer_addr(event)
+    eh.connect()
+    eh.register_block_event(block_event_callback)
+    all_ehs.append(eh)
+
+    request["targets"] = [peer]
+    request["block"] = genesis_block
+    request["tx_id"] = tx_id
 
     return request

commit a7522aace9e5d251792a6a943190a0ebb66b9e79

FAB-7751: fix MSP unkonw bug
This bug is introduced from 9a7d9e9f0.
When to join a peer into a channel, the local msp is used,
and the channel id is ignored. And now the join channel is work.

Change-Id: I153a348f4281968c54bcdf4a63c934a16adacaa2
Signed-off-by: Wang Dong <xdragon007@gmail.com>

hfc/fabric/channel/channel.py
@@ -16,12 +16,12 @@ from hfc.fabric.transaction.tx_proposal_request import \
 from hfc.protos.common import common_pb2
 from hfc.protos.orderer import ab_pb2
 from hfc.protos.peer import chaincode_pb2, proposal_pb2
-from hfc.protos.utils import create_seek_info, create_chaincode_spec, \
+from hfc.protos.utils import create_seek_info, create_cc_spec, \
     create_seek_payload, create_envelope
 from hfc.util import utils
 from hfc.util.utils import proto_str, current_timestamp, proto_b, \
     build_header, build_channel_header, build_cc_proposal, \
-    create_serialized_identity, send_transaction_proposal
+    send_transaction_proposal
 
 if sys.version_info < (3, 0):
     from Queue import Queue
@@ -507,7 +507,7 @@ class Channel(object):
         if(not request):
             err_msg = "Missing all required input request parameters"
 
-        if 'targets'not in request:
+        if 'targets' not in request:
             err_msg = "Missing peers parameter"
 
         if 'block' not in request:
@@ -521,27 +521,33 @@ class Channel(object):
             raise ValueError(err_msg)
 
         tx_context = self._client.tx_context
-        chaincode_spec = create_chaincode_spec(
-            request['block'],
-            'cscc',
-            "GOLANG")
+        block_bytes = request['block']
+        chaincode_input = chaincode_pb2.ChaincodeInput()
+        chaincode_input.args.extend(["JoinChain", block_bytes])
+        chaincode_id = chaincode_pb2.ChaincodeID()
+        chaincode_id.name = "cscc"
 
-        extension = chaincode_spec.SerializeToString()
+        cc_spec = create_cc_spec(chaincode_input, chaincode_id, 'GOLANG')
+        cc_invoke_spec = chaincode_pb2.ChaincodeInvocationSpec()
+        cc_invoke_spec.chaincode_spec.CopyFrom(cc_spec)
+
+        extension = proposal_pb2.ChaincodeHeaderExtension()
+        extension.chaincode_id.name = 'cscc'
         channel_header = build_channel_header(
             common_pb2.HeaderType.Value('ENDORSER_TRANSACTION'),
             tx_context.tx_id,
-            self.name,
+            '',
             current_timestamp(),
             tx_context.epoch,
-            extension=extension
-        )
+            extension=extension.SerializeToString())
 
-        creator = create_serialized_identity(tx_context.user)
-        header = build_header(creator, channel_header, tx_context.nonce)
+        header = build_header(tx_context.identity,
+                              channel_header,
+                              tx_context.nonce)
         proposal = build_cc_proposal(
-            chaincode_spec,
+            cc_invoke_spec,
             header,
-            {})
+            request['transient_map'])
 
         try:
             responses = send_transaction_proposal(
@@ -553,14 +559,16 @@ class Channel(object):
             raise IOError("fail to send transanction proposal", e)
 
         q = Queue(1)
+        result = True
         for r in responses:
             r.subscribe(on_next=lambda x: q.put(x),
                         on_error=lambda x: q.put(x))
             res = q.get(timeout=5)
+            proposal_res = res[0]
+            result = result and (proposal_res.response.status == 200)
+        if result:
+            _logger.info("sucessfully join the peers")
 
-            result = True and (res.response.status == 200)
-
-        _logger.info("The return status is:", result)
         return result
 
 

hfc/protos/utils.py
@@ -37,20 +37,14 @@ def create_seek_payload(seek_header, seek_info):
 
     return seek_payload_bytes
 
-def create_chaincode_spec(block_bytes, chaincode_id_name, type):
-
-    chaincode_input = chaincode_pb2.ChaincodeInput()
-    chaincode_input.args.append(block_bytes)
-
-    chaincode_id = chaincode_pb2.ChaincodeID()
-    chaincode_id.name = chaincode_id_name
+def create_cc_spec(chaincode_input, chaincode_id, type):
 
     chaincode_spec = chaincode_pb2.ChaincodeSpec()
     chaincode_spec.type = chaincode_pb2.ChaincodeSpec.Type.Value(type)
     chaincode_spec.chaincode_id.CopyFrom(chaincode_id)
     chaincode_spec.input.CopyFrom(chaincode_input)
 
-    return chaincode_spec.SerializeToString()
+    return chaincode_spec
 
 def create_tx_payload(endorsements, tran_req):
 

test/integration/e2e_test.py
@@ -83,25 +83,26 @@ class E2eTest(unittest.TestCase):
 
     def join_channel(self):
 
-        # sleep 5 seconds for channel created
+        # wait for channel created
         time.sleep(5)
         client = Client()
-        client.state_store = FileKeyValueStore(
-            self.kv_store_path + 'join-channel')
-
         channel = client.new_channel(self.channel_name)
 
         logger.info("start to join channel")
         orgs = ["org1.example.com", "org2.example.com"]
+        done = True
         for org in orgs:
+            client.state_store = FileKeyValueStore(
+                self.kv_store_path + org)
             request = build_join_channel_req(org, channel, client)
-            assert(request)
-            # result = True and channel.join_channel(request)
-            logger.info("peers in org: %s join channel: %s",
-                        org, self.channel_name)
-
-        logger.info("joining channel tested successfully")
+            done = done and channel.join_channel(request)
+            if done:
+                logger.info("peers in org: %s join channel: %s.",
+                            org, self.channel_name)
+        if done:
+            logger.info("joining channel tested succefully.")
         client.state_store = None
+        assert(done)
 
     def install_chaincode(self):
 

test/integration/e2e_utils.py
@@ -6,7 +6,6 @@
 
 from hfc.fabric.orderer import Orderer
 from hfc.fabric.peer import Peer
-from hfc.fabric.eventhub import EventHub
 from hfc.fabric.transaction.tx_context import TXContext
 from hfc.fabric.transaction.tx_proposal_request import TXProposalRequest
 from hfc.util.crypto.crypto import ecies
@@ -104,7 +103,6 @@ def build_join_channel_req(org, channel, client):
         pass
 
     client._crypto_suite = ecies()
-    all_ehs = []
     request = {}
     tx_prop_req = TXProposalRequest()
 
@@ -122,17 +120,21 @@ def build_join_channel_req(org, channel, client):
     tx_context = TXContext(orderer_admin, ecies(), tx_prop_req)
     client.tx_context = tx_context
     genesis_block = channel.get_genesis_block().SerializeToString()
-    if not genesis_block:
-        return None
 
     # create the peer
     org_admin = get_peer_org_user(client, org)
     client.tx_context = TXContext(org_admin, ecies(), tx_prop_req)
+    tx_id = client.tx_context.tx_id
 
     peer_config = test_network[org]["peers"]['peer0']
     ca_root = peer_config["tls_cacerts"]
-    peer = Peer(tls_cacerts=ca_root)
 
+    endpoint = peer_config["grpc_request_endpoint"]
+    opts = (('grpc.ssl_target_name_override',
+             peer_config['server_hostname']),)
+    peer = Peer(endpoint=endpoint, tls_cacerts=ca_root, opts=opts)
+
+    """
     # connect the peer
     eh = EventHub()
     event = peer_config['grpc_event_endpoint']
@@ -142,9 +144,11 @@ def build_join_channel_req(org, channel, client):
     eh.connect()
     eh.register_block_event(block_event_callback)
     all_ehs.append(eh)
+    """
 
     request["targets"] = [peer]
     request["block"] = genesis_block
     request["tx_id"] = tx_id
+    request["transient_map"] = {}
 
     return request