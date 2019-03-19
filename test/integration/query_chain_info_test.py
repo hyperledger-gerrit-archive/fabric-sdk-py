5399dd04dc826daa8301b39594cc4dbe0eec29eb
GuillaumeCisco <guillaumecisco@gmail.com>
2019-03-19 16:44:09 +0100

    [FABP-213] Support Async - Rework Event Hub
    Transform a lot of client methods into async function.
    Fix all methods which were only veryfing their result on the
    first response.
    Rework Event Hub for registering to a Tx Event.
    Add concept of waitForEvent for the chaincode instantiate.
    
    Change-Id: I7bfff1ade516e73ddd36df5a72e5a63bff2214a6
    Signed-off-by: GuillaumeCisco <guillaumecisco@gmail.com>

--- test/integration/query_chain_info_test.py
+++ test/integration/query_chain_info_test.py
@@ -1,20 +1,19 @@
 # Copyright IBM ALL Rights Reserved.
 #
 # SPDX-License-Identifier: Apache-2.0
-
+import asyncio
 import logging
-
 from hfc.fabric.peer import create_peer
 from hfc.fabric.transaction.tx_context import create_tx_context
 from hfc.fabric.transaction.tx_proposal_request import create_tx_prop_req, \
     CC_INVOKE, CC_TYPE_GOLANG, CC_INSTANTIATE, CC_INSTALL, TXProposalRequest
 from hfc.util.crypto.crypto import ecies
 from hfc.util.utils import build_tx_req, send_transaction
-from test.integration.utils import get_peer_org_user,\
+from test.integration.utils import get_peer_org_user, \
     BaseTestCase
 from test.integration.config import E2E_CONFIG
-from test.integration.e2e_utils import build_channel_request,\
-    build_join_channel_req
+from test.integration.e2e_utils import build_channel_request, \
+    build_join_channel_req, get_stream_result
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -24,9 +23,9 @@ CC_NAME = 'example_cc'
 CC_VERSION = '1.0'
 
 
-class QueryBlockTest(BaseTestCase):
-    def invoke_chaincode(self):
+class QueryChainInfoTest(BaseTestCase):
 
+    async def invoke_chaincode(self):
         self.channel = self.client.new_channel(self.channel_name)
         org1 = "org1.example.com"
         peer_config = test_network['org1.example.com']['peers']['peer0']
@@ -84,36 +83,48 @@ class QueryBlockTest(BaseTestCase):
         request = build_channel_request(self.client,
                                         self.channel_tx,
                                         self.channel_name)
-        self.client._create_channel(request)
+        await self.client._create_channel(request)
 
-        join_req = build_join_channel_req(org1, self.channel, self.client)
-        self.channel.join_channel(join_req)
+        join_req = await build_join_channel_req(org1, self.channel,
+                                                self.client)
+        responses = self.channel.join_channel(join_req)
+        res = await asyncio.gather(*responses)
+        self.assertTrue(all([x.response.status == 200 for x in res]))
 
-        self.client.send_install_proposal(tx_context_install, [self.org1_peer])
+        responses, proposal, header = self.client.send_install_proposal(
+            tx_context_install, [self.org1_peer])
+        await asyncio.gather(*responses)
 
-        res = self.channel.send_instantiate_proposal(tx_context_dep,
-                                                     [self.org1_peer])
+        responses, proposal, header = self.channel.send_instantiate_proposal(
+            tx_context_dep,
+            [self.org1_peer])
+        res = await asyncio.gather(*responses)
+        tran_req = build_tx_req((res, proposal, header))
 
-        tran_req = build_tx_req(res)
-        send_transaction(self.channel.orderers, tran_req, tx_context)
+        await get_stream_result(
+            send_transaction(self.channel.orderers, tran_req, tx_context))
 
         tx_context_tx = create_tx_context(self.org1_admin,
                                           crypto,
                                           TXProposalRequest())
-        res = self.channel.send_tx_proposal(tx_context, [self.org1_peer])
+        responses, p, h = self.channel.send_tx_proposal(tx_context,
+                                                        [self.org1_peer])
+        res = await asyncio.gather(*responses)
+        tran_req = build_tx_req((res, p, h))
 
-        tran_req = build_tx_req(res)
+        await get_stream_result(
+            send_transaction(self.channel.orderers, tran_req, tx_context_tx))
 
-        send_transaction(self.channel.orderers, tran_req, tx_context_tx)
+    def test_query_installed_chaincodes_sucess(self):
+        loop = asyncio.get_event_loop()
 
-    def test_query_block_success(self):
-
-        self.invoke_chaincode()
+        loop.run_until_complete(self.invoke_chaincode())
 
         tx_context = create_tx_context(self.org1_admin,
                                        ecies(),
                                        TXProposalRequest())
-        response = self.channel.query_block(tx_context,
-                                            [self.org1_peer],
-                                            "1")
-        self.assertEqual(response[0][0].response.status, 200)
+        responses, proposal, header = self.channel.query_info(tx_context,
+                                                              [self.org1_peer])
+        res = loop.run_until_complete(asyncio.gather(*responses))
+
+        self.assertEqual(res[0].response.status, 200)
