da8d22e7ba858af02571c0b4015a19e4dbb9614a
Brett Logan <Brett.T.Logan@ibm.com>
2019-05-23 15:06:08 -0400

    [FAB-15552] Remove fabric-baseimage
    Signed-off-by: Brett Logan <Brett.T.Logan@ibm.com>
    Change-Id: I69ed2c8d024348b83b2d2d9333e67fdb0b97b4b4

--- scripts/check_env.sh
+++ scripts/check_env.sh
@@ -45,12 +45,6 @@ IMG_TAG=1.4.0
 echo "=====> Pulling fabric Images"
 dockerFabricPull ${FABRIC_TAG}
 
-IMG=hyperledger/fabric-baseimage:$BASEIMAGE_RELEASE
-[ -z "$(docker images -q ${IMG} 2> /dev/null)" ] && docker pull ${IMG}
-
-IMG=hyperledger/fabric-baseos:$BASEIMAGE_RELEASE
-[ -z "$(docker images -q ${IMG} 2> /dev/null)" ] && docker pull ${IMG}
-
 if ! which configtxgen
 then
     if  [ ! -e fabric-bin/bin/configtxgen ];
